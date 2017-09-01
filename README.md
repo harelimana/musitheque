d806162434bf72815bf718ee2b33ef8c05172eed
axxa <alexandre.harelimana@gmail.com>
2017-09-01 19:50:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+musitheque
+==========
+
+A Symfony project created on September 1, 2017, 6:21 pm.
+# musitheque
---
+++ src/AppBundle/Form/AlbumsType.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: axxa
+ * Date: 01.09.17
+ * Time: 19:30
+ */
+
+class AlbumsType
+{
+    
+}
