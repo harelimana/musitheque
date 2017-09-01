342fccd455d6260ae3cd0433ca53b45add6ce5d6
axxa <alexandre.harelimana@gmail.com>
2017-09-01 19:53:31 +0200

    new file:   composer.json 	new file:   composer.lock 	new file:   phpunit.xml.dist 	modified:   src/AppBundle/Form/AlbumsType.php 	modified:   src/AppBundle/Form/CategoriesType.php 	app/ 	bin/ 	src/.htaccess 	src/AppBundle/AppBundle.php 	src/AppBundle/Controller/ 	src/AppBundle/Entity/ 	src/AppBundle/Repository/ 	src/AppBundle/Resources/ 	src/AppBundle/Tests/ 	tests/ 	var/

--- src/AppBundle/Form/AlbumsType.php
+++ src/AppBundle/Form/AlbumsType.php
@@ -6,7 +6,27 @@
  * Time: 19:30
  */
 
-class AlbumsType
+namespace AppBundle\Form;
+
+use AppBundle\Entity\albums;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AlbumsType extends AbstractType
 {
-    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('release_date', TextType::class)
+            ->add('album_art', TextType::class);
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => albums::class,
+        ));
+    }
 }
--- src/AppBundle/Form/CategoriesType.php
+++ src/AppBundle/Form/CategoriesType.php
@@ -6,7 +6,26 @@
  * Time: 19:30
  */
 
-class CategoriesType
+namespace AppBundle\Form;
+
+use AppBundle\Entity\categories;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoriesType extends AbstractType
 {
-    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('description', TextType::class);
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => categories::class,
+        ));
+    }
 }
